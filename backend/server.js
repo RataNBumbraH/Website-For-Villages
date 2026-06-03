20aa923d438d590050766cb718eb65bd1d16d5e0
Ratan Bumbrah <ratansahib41@gmail.com>
2026-06-03 22:27:34 +0530

    Updated cors conf

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:"http://localhost:3000",
+        origin:["http://localhost:3000", "website-for-villages.vercel.app"],
         methods:['GET','POST','PUT','DELETE'],
         allowedHeaders:['Content-Type','Authorization'],
         credentials:true
